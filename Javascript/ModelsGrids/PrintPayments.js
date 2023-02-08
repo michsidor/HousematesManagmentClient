35163d62c4192a75e5f8a35d91413b6ab4373b50
MichalSidor <michsidor@wp.pl>
2023-02-08 09:07:37 +0100

    finished updating funcionality for all models

--- Javascript/ModelsGrids/PrintPayments.js
+++ Javascript/ModelsGrids/PrintPayments.js
@@ -8,11 +8,15 @@ window.onload = function() {
     item.Amount + 
     item.Deadline + 
     item.DebtorsMetadata +
-    "<input type='button' onclick='ObjectDelete(" + '"https://localhost:7021/api/payment/' + item.Id +  '"'  + ")'value='Delete payment'></input>" +
-    "<input type='button' onclick='ObjectDelete(" + '"https://localhost:7021/api/payment/' + item.Id +  '"'  + ")'value='Edit payment'></input>" +       
-    "</li>").join("") + 
-    "</h3>" +"<br>";
+        "<input type='button' onclick='ObjectDelete(" + '"https://localhost:7021/api/payment/' + item.Id +  '"'  + ")'value='Delete payment'></input>" +
+        "<input type='button' onclick='ObjectEdit(" + '"' + item.Id + '"' + ")'value='Edit payment'></input>" + 
+        "</li>").join("") + 
+        "</h3>" +"<br>";
     console.log(data);
 
     document.getElementById("result").innerHTML = resultHTML;
 };
+
+function ObjectEdit(Id){
+    window.location.href = '/HTML/UpdateModels/UpdatePayment.html?id='+Id;
+}
---
+++ Javascript/Requests/PostRequest.js
@@ -0,0 +1,67 @@
+window.onload = async function(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get("id");
+    const formPayment = document.getElementById("editPayment");
+    const formAdvertisement = document.getElementById("editAdvertisement");
+    const formAssignment = document.getElementById("editAssignment");
+    if(formPayment != null){
+        formPaymentFunction("https://localhost:7021/api/payment",formPayment,id);
+    }
+    else if(formAdvertisement != null){
+        formAssignmentAndAdvertisementFunction("https://localhost:7021/api/advertisement",formAdvertisement,id);
+    }
+    else if(formAssignment != null){
+        formAssignmentAndAdvertisementFunction("https://localhost:7021/api/assignment",formAssignment,id);
+    }
+}
+
+const formPaymentFunction = (url,form,Id) => {
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const Amount = form.elements.amount.value;
+        const Deadline = form.elements.deadline.value;
+        const DebtorsMetadata = form.elements.debtors.value;
+        const formData = {
+            Id, 
+            Amount,
+            Deadline,
+            DebtorsMetadata
+        };
+        debugger;
+        await ObjectEdit(url, formData);
+    });
+}
+
+const formAssignmentAndAdvertisementFunction = (url,form,Id) => {
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const Title = form.elements.title.value;
+        const Description = form.elements.description.value;
+        const Comments = form.elements.comments.value;
+        const Status = false;
+        const formData = {
+            Id,
+            Title,
+            Description,
+            Comments,
+            Status
+        };
+
+        await ObjectEdit(url, formData);
+    });
+}
+
+const ObjectEdit = async(url,body) => {
+    await PostMethod(url,body);
+    window.location.href='/HTML/HousematesManagment.html'
+}
+
+const PostMethod = async(url,body) => {
+    const reposne = await fetch(url,{
+         method:"POST",
+         headers: {
+            "Content-Type": "application/json;charset=utf-8"
+        },
+        body: JSON.stringify(body)
+    });
+}
