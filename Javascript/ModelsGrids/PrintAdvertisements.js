c24c42f2f157de24c2376c1a9abe081ddc731b59
MichalSidor <michsidor@wp.pl>
2023-02-10 23:10:46 +0100

    edited grid stylesheet

--- Javascript/ModelsGrids/PrintAdvertisements.js
+++ Javascript/ModelsGrids/PrintAdvertisements.js
@@ -1,18 +1,26 @@
 window.onload = function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const data = JSON.parse(urlParams.get("data"));
+  const urlParams = new URLSearchParams(window.location.search);
+  const data = JSON.parse(urlParams.get("data"));
 
-    const resultHTML = "<h3>" + data.map(item => 
-    "<li>" + 
-    item.Id + 
-    item.Title + 
-    item.Description + 
-    item.Comments +
-    "<input type='button' onclick='ObjectDelete(" + '"https://localhost:7021/api/advertisement/' + item.Id +  '"'  + ")'value='Delete advertisement'></input>" +
-    "<input type='button' onclick='ObjectDelete(" + '"https://localhost:7021/api/advertisement/' + item.Id +  '"'  + ")'value='Edit payment'></input>" +
-    "</li>").join("") + 
-    "</h3>" +"<br>";
-    console.log(data);
-    
-    document.getElementById("result").innerHTML = resultHTML;
-  };
+  const resultHTML = data.map(item => 
+      "<table class='styled-table'>" + 
+      "<thead><tr><th>Title</th><th>Description</th><th>Comments</th></tr></thead>"+
+      "<tbody>"+
+      "<tr class='active-row'>" + 
+      "<td>" + item.Title + "</td>" +
+      "<td>" + item.Description + "</td>" + 
+      "<td>" + item.Comments + "</td>" + 
+      "<td>" + 
+          "<input type='button' onclick='ObjectDelete(" + '"https://localhost:7021/api/advertisement/' + item.Id +  '"'  + ")'value='Delete advertisement'></input>" +
+          "<input type='button' onclick='ObjectEdit(" + '"' + item.Id + '"' + ")'value='Edit advertisement'></input>" + 
+      "</td>" +
+      "</tr>").join("") + 
+  "</table>";
+  console.log(data);
+
+  document.getElementById("result").innerHTML = resultHTML;
+};
+
+function ObjectEdit(Id){
+  window.location.href = '/HTML/UpdateModels/UpdateAdvertisement.html?id='+Id;
+}
--- Javascript/ModelsGrids/PrintPayments.js
+++ Javascript/ModelsGrids/PrintPayments.js
@@ -2,16 +2,20 @@ window.onload = function() {
     const urlParams = new URLSearchParams(window.location.search);
     const data = JSON.parse(urlParams.get("data"));
 
-    const resultHTML = "<h3>" + data.map(item => 
-    "<li>" + 
-    item.Id + 
-    item.Amount + 
-    item.Deadline + 
-    item.DebtorsMetadata +
-        "<input type='button' onclick='ObjectDelete(" + '"https://localhost:7021/api/payment/' + item.Id +  '"'  + ")'value='Delete payment'></input>" +
-        "<input type='button' onclick='ObjectEdit(" + '"' + item.Id + '"' + ")'value='Edit payment'></input>" + 
-        "</li>").join("") + 
-        "</h3>" +"<br>";
+    const resultHTML = data.map(item => 
+        "<table class='styled-table'>" + 
+        "<thead><tr><th>Amount</th><th>Deadline</th><th>Debtors</th></tr></thead>"+
+        "<tbody>"+
+        "<tr class='active-row'>" + 
+        "<td>" + item.Amount + "</td>" +
+        "<td>" + item.Deadline + "</td>" + 
+        "<td>" + item.DebtorsMetadata + "</td>" + 
+        "<td>" + 
+            "<input type='button' onclick='ObjectDelete(" + '"https://localhost:7021/api/payment/' + item.Id +  '"'  + ")'value='Delete payment'></input>" +
+            "<input type='button' onclick='ObjectEdit(" + '"' + item.Id + '"' + ")'value='Edit payment'></input>" + 
+        "</td>" +
+        "</tr>").join("") + 
+    "</table>";
     console.log(data);
 
     document.getElementById("result").innerHTML = resultHTML;
